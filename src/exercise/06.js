b3b9cead2d444deacf3a8f8b9bf43e83d7923b2a
BorisPT <60671079+BorisPT@users.noreply.github.com>
2022-08-18 11:02:51 +0100

    Fetching pokemon data

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,14 +6,44 @@ import * as React from 'react'
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {PokemonForm} from '../pokemon';
+import {PokemonDataView} from '../pokemon';
+import {PokemonInfoFallback} from "../pokemon";
+import {fetchPokemon} from "../pokemon";
 
 function PokemonInfo({pokemonName}) {
+
+  const [pokemonInfo, setPokemonInfo] = React.useState(null);
+
+  React.useEffect(() => { 
+
+    const getPokemonInfo = async (pokeName) => { 
+
+      setPokemonInfo(null);
+
+      if (!pokeName)
+      {        
+        return;
+      }      
+
+      const data = await fetchPokemon(pokeName);
+      console.log("Data:", data);
+      console.dir(data);
+      setPokemonInfo(data);
+    
+     }
+
+     getPokemonInfo(pokemonName);
+
+   }, [pokemonName]);
+
+
   // 🐨 Have state for the pokemon (null)
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
   // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
   // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
+
   // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
   // (This is to enable the loading state when switching between different pokemon.)
   // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
@@ -25,8 +55,24 @@ function PokemonInfo({pokemonName}) {
   //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
   //   3. pokemon: <PokemonDataView pokemon={pokemon} />
 
-  // 💣 remove this
-  return 'TODO'
+  let content = <p>Please submit a pokemon</p>;
+
+  if (pokemonName)
+  {
+    if (pokemonInfo !== null)
+    {
+      console.log("Setting content for pokemon data view");
+      content = <PokemonDataView pokemon={pokemonInfo} />;
+    }
+    else
+    {
+      content = <PokemonInfoFallback name={pokemonName} />;
+    }
+  }
+  
+  return (
+    content
+    );
 }
 
 function App() {
